b4b2e216d991032c8bb7937852b9829161cd353a
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-12-14 16:13:39 +0900

    update gist-client

--- gist-client/src/lib.rs
+++ gist-client/src/lib.rs
@@ -2,12 +2,12 @@
 
 use chrono::{DateTime, Utc};
 use http::{
-    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, ETAG, IF_NONE_MATCH},
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, ETAG, IF_MATCH, IF_NONE_MATCH},
     HeaderValue, Request, StatusCode,
 };
 use isahc::RequestExt;
 use mime::Mime;
-use serde::Deserialize;
+use serde::{ser::SerializeMap, Deserialize, Serialize, Serializer};
 use std::collections::HashMap;
 
 /// The entity tag to specify the revision of Gist content.
@@ -75,6 +75,63 @@ impl Client {
 
         Ok(Some((gist, etag)))
     }
+
+    /// Edit the content of a Gist file.
+    ///
+    /// https://developer.github.com/v3/gists/#edit-a-gist
+    pub async fn update_gist(
+        &self,
+        gist_id: &str,
+        etag: Option<&ETag>,
+        patch: GistPatch<'_>,
+    ) -> anyhow::Result<(Gist, Option<ETag>)> {
+        let response = {
+            let url = format!("https://api.github.com/gists/{id}", id = gist_id);
+            let mut request = Request::patch(url);
+            // TODO: specify the custom media types
+            // https://developer.github.com/v3/gists/#custom-media-types
+            request.header(ACCEPT, "application/vnd.github.v3+json");
+            request.header(CONTENT_TYPE, "application/json; charset=utf-8");
+            if let Some(ref token) = self.token {
+                request.header(AUTHORIZATION, format!("token {token}", token = token));
+            }
+
+            if let Some(etag) = etag {
+                request.header(IF_MATCH, &etag.0);
+            }
+
+            request
+                .body(serde_json::to_string(&patch)?)?
+                .send_async()
+                .await?
+        };
+
+        match response.status() {
+            StatusCode::OK => (),
+            StatusCode::NOT_FOUND => return Err(anyhow::anyhow!("The Gist is not found")),
+            StatusCode::PRECONDITION_FAILED => {
+                return Err(anyhow::anyhow!("The Gist has been edited by someone."))
+            }
+            status => return Err(anyhow::anyhow!("API error: {}", status)),
+        }
+
+        if let Some(content_type) = response.headers().get(CONTENT_TYPE) {
+            let mime: Mime = content_type.to_str()?.parse()?;
+            anyhow::ensure!(
+                mime.type_() == "application" && mime.subtype() == "json",
+                "content type is not JSON"
+            );
+        }
+
+        let etag = response.headers().get(ETAG).map(|etag| ETag(etag.clone()));
+
+        let body = response.into_body().text_async().await?;
+        let gist: Gist = serde_json::from_str(&body)?;
+
+        anyhow::ensure!(gist.id == gist_id, "Gist ID is mismatched");
+
+        Ok((gist, etag))
+    }
 }
 
 /// A Gist received from the server.
@@ -113,4 +170,47 @@ where
 {
     let s = String::deserialize(de)?;
     s.parse().map_err(serde::de::Error::custom)
+}
+
+pub struct GistPatch<'a> {
+    pub files: &'a [(&'a str, Option<&'a str>)],
+    pub description: Option<&'a str>,
+}
+
+impl Serialize for GistPatch<'_> {
+    fn serialize<S>(&self, se: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = se.serialize_map(Some(2))?;
+        map.serialize_entry("files", &self.files)?;
+        if let Some(description) = self.description {
+            map.serialize_entry("description", description)?;
+        }
+        map.end()
+    }
+}
+
+struct GistPatchFiles<'a>(&'a [(&'a str, Option<&'a str>)]);
+
+impl Serialize for GistPatchFiles<'_> {
+    fn serialize<S>(&self, se: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = se.serialize_map(Some(self.0.len()))?;
+        for &(filename, content) in self.0 {
+            map.serialize_entry(
+                filename,
+                &content.map(|content| GistPatchFile { filename, content }),
+            )?;
+        }
+        map.end()
+    }
+}
+
+#[derive(Serialize)]
+struct GistPatchFile<'a> {
+    filename: &'a str,
+    content: &'a str,
 }
